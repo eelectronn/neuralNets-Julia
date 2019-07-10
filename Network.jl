76f58a8d0b9d6c66e73ff1d279bde88f4e3f14ef
harshpatel <patel-h91@webmail.uwinnipeg.ca>
2019-07-10 02:48:04 -0500

    Constructor for network has been made specific by providing type of parameter. Bias is now being generated as column vector instead of row vector. Forward feed converts input into from row vector to column vector first. getGradient method has been implemented. Sigmoid and sigmoidPrime function has been added. Driver has been modified to for testing the getGradient function.

--- Network.jl
+++ Network.jl
@@ -2,16 +2,44 @@ mutable struct Network
     numOfLayers::Int
     weight::Array
     bias::Array
-    function Network(layers)
+    function Network(layers::Array)
         numOfLayers = length(layers)
         weight = [rand(layers[i], layers[i-1]) for i = 2:length(layers)]
-        bias = [rand(layers[i]) for i = 2:length(layers)]
+        bias = [rand(layers[i], 1) for i = 2:length(layers)]
         return new(numOfLayers, weight, bias)
     end
 end
+
 function forwardFeed(net::Network, inp::Array)
+    inp = sigmoid.(Matrix(transpose(inp)))
     for i = 1:length(net.weight)
-        inp = net.weight[i]*inp + net.bias[i]
+        inp = sigmoid.(net.weight[i]*inp + net.bias[i])
     end
-    return inp
+    return Matrix(transpose(inp))
+end
+
+function getGradient(net::Network, data::Array, expected::Array)
+    expected = Matrix(transpose(expected))
+    activations, zs = [sigmoid.(Matrix(transpose(data)))], [sigmoid.(Matrix(transpose(data)))]
+    for (weightSet, biasSet) in zip(net.weight, net.bias)
+        push!(zs, weightSet*activations[length(activations)]+biasSet)
+        push!(activations, sigmoid.(zs[length(zs)]))
+    end
+    delta = 2*(activations[length(activations)]-expected)
+    delta_w, delta_b = [], []
+    for i = net.numOfLayers:-1:2
+        z_prime = sigmoidPrime.(zs[i])
+        pushfirst!(delta_b, delta.*z_prime)
+        pushfirst!(delta_w, delta_b[1]*Matrix(transpose(activations[i-1])))
+        delta = Matrix(transpose(Matrix(transpose(delta_b[1]))*net.weight[i-1]))
+    end
+    return delta_w, delta_b
+end
+
+function sigmoid(num)
+    return 1.0/(1.0+MathConstants.e^(-num))
+end
+
+function sigmoidPrime(num)
+    return (sigmoid(num)^2)*MathConstants.e^(-num)
 end
